7080e2c1d74bd0f8d3b91d7f9217a1a2273c6f88
Charles Cozad <ccozad@gmail.com>
2013-11-09 09:43:41 -0800

    - Removed the random rectangle being displayed

--- Jaalaga/src/com/johnadamsacademy/mentor/technologydevelopment/jaalaga/GameLevel.java
+++ Jaalaga/src/com/johnadamsacademy/mentor/technologydevelopment/jaalaga/GameLevel.java
@@ -1,8 +1,5 @@
 package com.johnadamsacademy.mentor.technologydevelopment.jaalaga;
 
-import org.andengine.entity.primitive.Rectangle;
-import org.andengine.util.math.MathUtils;
-
 //This software is licensed under The MIT License (MIT)
 //
 //Copyright (c) 2013 Charles Cozad
@@ -40,13 +37,5 @@ public class GameLevel extends ManagedGameScene {
 	@Override
 	public void onLoadScene() {
 		super.onLoadScene();
-		Rectangle rectangle = new Rectangle(
-				0f,
-				0f,
-				120f,
-				120f,
-				this.getResourceManager().getEngine().getVertexBufferObjectManager());
-		rectangle.setPosition(MathUtils.random(0f+rectangle.getWidth(),(800f-rectangle.getWidth())), MathUtils.random((-240f+rectangle.getHeight()),(240f-rectangle.getHeight())));
-		this.attachChild(rectangle);
 	}
 }
